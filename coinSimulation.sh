fdef3c307e039605a9c62e68b3afa5a7702e7104
Komal <divyavairagade@gmail.com>
2022-04-01 19:35:02 +0530

    used for loop

--- coinSimulation.sh
+++ coinSimulation.sh
@@ -1,9 +1,21 @@
 #!/bin/bash -x
 
-coin=$((RANDOM%2))
-if [ $coin -eq 0 ]
-then
-	echo "head";
-else
-	echo "tail";
-fi
+headcount=0;
+tailcount=0;
+
+
+for (( i=0; i<=10; i++ ))
+do
+	coin=$((RANDOM%2))
+	if [ $coin -eq 0 ]
+	then
+		echo "head";
+		((headcount++));
+	else
+		echo "tail";
+		((tailcount++));
+	fi
+done
+
+echo "head wins : $headcount times";
+echo "tail wins : $tailcount times";
